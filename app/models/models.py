a3b033187c3612aa2f269f3fcef0f4b3ccd3e32a
aruiz <aruiz@grupooctano.com.mx>
2023-10-26 11:01:50 -0600

    Clase 16 Registrar usuario

--- app/models/models.py
+++ app/models/models.py
@@ -3,7 +3,7 @@ from database import db
 class Usuario(db.Model):
   __tablename__ = 'usuarios'
   id = db.Column(db.Integer, primary_key=True)
-  usuario = db.Column(db.String(70), nulllable=False, unique=True)
+  usuario = db.Column(db.String(70), nullable=False, unique=True)
   clave = db.Column(db.String(100), nullable=False)
   
   def __init__(self, usuario, clave):
